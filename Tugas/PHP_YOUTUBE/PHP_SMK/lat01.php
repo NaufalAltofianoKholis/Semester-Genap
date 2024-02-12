0a02abc283315181e79d009b8d806c82275ef68d
NaufalAltofianoKholis <140329480+NaufalAltofianoKholis@users.noreply.github.com>
2024-02-12 11:16:57 +0700

    Update lat01.php

--- Tugas/PHP_YOUTUBE/PHP_SMK/lat01.php
+++ Tugas/PHP_YOUTUBE/PHP_SMK/lat01.php
@@ -1,5 +1,15 @@
 <?php
 
+echo "Belajar PHP";
+echo '<br>';
+echo 'Saya Siswa SMK';
+echo '<h1  style = "background-color: red">Belajar PHP itu Mudah</h1>
+        <p style = "background-color: aqua"> Saya suka belajar program</p>
+';
 
+echo 'Saya Belajar'.'PHP'.'<br>';
+
+echo 'Tahun 2024';
 
 ?>
+
